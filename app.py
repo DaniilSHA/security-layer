3fb9bf8ecacd20dd32771922130ad896dafcd50e
Daniil_Shakhmovedev <daniil.shakhmovedev@yandex.ru>
2024-08-16 00:55:33 +0300

    cookie_base ..

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from classes import Storage, AesEncrypter, AuthService, server
+import codecs
+import pickle
+
+from classes import Storage, AesEncrypter, AuthService, server, User
 
 storage = Storage()
 aes_encrypter = AesEncrypter()
 auth_service = AuthService(storage, aes_encrypter)
 server = server(auth_service)
+
+fail_user = User("admin", "faik", "admin")
+dump = pickle.dumps(fail_user)
+user_str = codecs.encode(dump, 'base64').decode()
+faik_cookie = aes_encrypter.encrypt(user_str)
+print(faik_cookie)
--- classes.py
+++ classes.py
@@ -1,8 +1,11 @@
+import binascii
+import codecs
 import hashlib
 import pickle
 
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
 
 from flask import Flask, request, after_this_request
 
@@ -16,9 +19,9 @@ class User:
 
 class Storage:
     store = {
-        "user": User("user", hashlib.md5(b"user").digest(), "user"),
-        "admin": User("admin", hashlib.md5(b"admin").digest(), "admin"),
-        "moder": User("moder", hashlib.md5(b"moder").digest(), "moder")
+        'user': User('user', hashlib.md5(b'user').digest(), 'user'),
+        'admin': User('admin', hashlib.md5(b'admin').digest(), 'admin'),
+        'moder': User('moder', hashlib.md5(b'moder').digest(), 'moder')
     }
 
     def login(self, username, password):
@@ -27,7 +30,7 @@ class Storage:
 
     def registration(self, username, password):
         psw_hash = hashlib.md5(bytes(password, 'utf-8')).digest()
-        self.store[username] = User(username, psw_hash, "user")
+        self.store[username] = User(username, psw_hash, 'user')
 
     def has_username(self, username):
         return username in self.store
@@ -42,17 +45,22 @@ class Storage:
 class AesEncrypter:
 
     def __init__(self):
-        self.key = get_random_bytes(16)
+        self.key = b'javajavajavajava'
 
-    def encrypt(self, decrypt_data):
-        cipher = AES.new(self.key, AES.MODE_CBC, 'init_val'.encode("utf8"))
-        encrypt_data = cipher.encrypt(decrypt_data)
-        return encrypt_data
+    def encrypt(self, decrypt_data_str):
+        cipher = AES.new(self.key, AES.MODE_CBC, 'init_val_is_16by'.encode('utf8'))
+        padded_data = pad(bytes(decrypt_data_str, 'utf8'), AES.block_size)
+        encrypt_data = cipher.encrypt(padded_data)
+        encrypt_data_str = binascii.hexlify(encrypt_data).decode()
+        return encrypt_data_str
 
-    def decrypt(self, encrypt_data):
-        cipher = AES.new(self.key, AES.MODE_CBC, 'init_val'.encode("utf8"))
+    def decrypt(self, encrypt_data_str):
+        encrypt_data = binascii.unhexlify(encrypt_data_str)
+        cipher = AES.new(self.key, AES.MODE_CBC, 'init_val_is_16by'.encode('utf8'))
         decrypt_data = cipher.decrypt(encrypt_data)
-        return decrypt_data
+        un_padded_data = unpad(decrypt_data, AES.block_size)
+        decrypt_data_str = str(un_padded_data, 'utf8')
+        return decrypt_data_str
 
 class AuthService:
     def __init__(self, storage, aes_encrypter):
@@ -62,7 +70,8 @@ class AuthService:
     def login(self, username, password):
         if self.storage.login(username, password):
             user = self.storage.find_user(username)
-            user_str = str(pickle.dumps(user), 'utf-8')
+            dump = pickle.dumps(user)
+            user_str = codecs.encode(dump, 'base64').decode()
             return self.aes_encrypter.encrypt(user_str)
         else:
             return ''
@@ -71,26 +80,34 @@ class AuthService:
         if not self.storage.has_username(username):
             self.storage.registration(username, password)
         else:
-            raise Exception("username is busy")
+            raise Exception('username is busy')
 
     def is_auth(self, session_id):
         user_str = self.aes_encrypter.decrypt(session_id)
-        user = pickle.loads(bytes(user_str, 'utf-8'))
-        return user
+        load = codecs.decode(user_str.encode(), 'base64')
+        user_from_session_id = pickle.loads(load)
+        if self.storage.find_user(user_from_session_id.username):
+            user_from_storage = self.storage.find_user(user_from_session_id.username)
+            if user_from_session_id.psw_hash == user_from_storage.psw_hash:
+                return user_from_storage
+            else:
+                return None
+        else:
+            return None
 
 
 def server(auth_service):
     app = Flask(__name__)
 
-    @app.route("/registration", methods=['POST'])
+    @app.route('/registration', methods=['POST'])
     def registration():
         r = request.json
         username = r['username']
         password = r['password']
         auth_service.registration(username, password)
-        return "ok"
+        return 'ok'
 
-    @app.route("/login", methods=['POST'])
+    @app.route('/login', methods=['POST'])
     def login():
         r = request.json
         username = r['username']
@@ -102,9 +119,9 @@ def server(auth_service):
             response.set_cookie('session_id', str(session_id), max_age=10000)
             return response
 
-        return "ok"
+        return 'ok'
 
-    @app.route("/logout", methods=['GET'])
+    @app.route('/logout', methods=['GET'])
     def logout():
 
         @after_this_request
@@ -112,18 +129,18 @@ def server(auth_service):
             response.delete_cookie('session_id')
             return response
 
-        return "logout ok"
+        return 'logout ok'
 
-    @app.route("/load", methods=['GET'])
+    @app.route('/load', methods=['GET'])
     def load():
         if 'session_id' in request.cookies:
             session_id = request.cookies['session_id']
             user = auth_service.is_auth(session_id)
             if user is None:
-                raise Exception("not auth1")
+                raise Exception('not auth1')
             else:
-                return "success, your role: " + user.role
+                return 'success, your role: ' + user.role
         else:
-            raise Exception("not auth2")
+            raise Exception('not auth2')
 
     return app
