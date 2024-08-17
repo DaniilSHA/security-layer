d58ea086215b089414cbba9ce17afd0a32a4f915
Daniil_Shakhmovedev <daniil.shakhmovedev@yandex.ru>
2024-08-17 03:02:48 +0300

    api-key ..

--- classes.py
+++ classes.py
@@ -3,6 +3,7 @@ import codecs
 import hashlib
 import pickle
 
+import requests
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
@@ -142,5 +143,44 @@ def server(auth_service):
                 return 'success, your role: ' + user.role
         else:
             raise Exception('not auth2')
+
+    @app.route('/gen-api-key', methods=['POST'])
+    def gen_api_key():
+        if 'session_id' in request.cookies:
+            session_id = request.cookies['session_id']
+            user = auth_service.is_auth(session_id)
+            if user is None:
+                raise Exception('not auth1')
+            else:
+                r = request.json
+                password = r['password']
+                return auth_service.login(user.username, password)
+        else:
+            raise Exception('not auth2')
+
+    @app.route('/load-on-api-key', methods=['POST'])
+    def load_on_api_key():
+        r = request.json
+        if 'api-key' in r:
+            api_key = r['api-key']
+            user = auth_service.is_auth(api_key)
+            if user is None:
+                raise Exception('not auth1')
+            else:
+                return 'api key info: ' + str(user.psw_hash)
+        else:
+            raise Exception('not auth2')
+
+    @app.route('/get-data-on-api-key', methods=['POST'])
+    def get_data_on_api_key():
+        r = request.json
+        if 'api-key' in r:
+            api_key = r['api-key']
+            data = {'api-key': api_key}
+            headers = {'Content-type': 'application/json'}
+            response = requests.post('http://localhost:8082/load-on-api-key', json=data, headers=headers)
+            return response.content
+        else:
+            raise Exception('not api-key')
 
     return app
